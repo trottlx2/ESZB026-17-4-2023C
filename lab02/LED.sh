193e0357dee56ab9486974e3b22679c3b29a18ed
Louise Trottier <114197794+trottlx2@users.noreply.github.com>
2023-06-14 22:30:12 -0300

    Update LED.sh

--- lab02/LED.sh
+++ lab02/LED.sh
@@ -4,12 +4,17 @@
 # Derek Molloy, Exploring Raspberry Pi: Interfacing to the Real World with Embedded Linux,
 # Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/
 
-LED_GPIO=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+LED_GPIO1=20 #vermelho
+LED_GPIO2=21 #verde
+LED_GPIO3=16 #amarelo
+# Usar uma variavel facilita alteracoes futuras na porta usada
 
 # funcoes Bash
 function setLED
 {                                      # $1 eh o primeiro argumento passado para a funcao
-	echo $1 >> "/sys/class/gpio/gpio$LED_GPIO/value"
+	echo $1 >> "/sys/class/gpio/gpio$LED_GPIO1/value"
+ 	echo $2 >> "/sys/class/gpio/gpio$LED_GPIO2/value"
+  	echo $3 >> "/sys/class/gpio/gpio$LED_GPIO3/value"
 }
 
 # Inicio do programa
@@ -23,23 +28,49 @@ fi
 echo "O comando passado foi: $1"
 
 if [ "$1" == "setup" ]; then
-	echo "Habilitando a GPIO numero $LED_GPIO"
-	echo $LED_GPIO >> "/sys/class/gpio/export"
+	echo "Habilitando a GPIO numero $LED_GPIO1"
+	echo $LED_GPIO1 >> "/sys/class/gpio/export"
 	sleep 1                        # esperar 1 segundo para garantir que a gpio foi exportada
-	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO/direction"
-elif [ "$1" == "on" ]; then
+	
+ 
 	echo "Acendendo o LED"
 	setLED 1                       # 1 eh recebido como $1 na funcao setLED
-elif [ "$1" == "off" ]; then
+
+	sleep 2   
 	echo "Desligando o LED"
 	setLED 0                       # 0 eh recebido como $1 na funcao setLED
-elif [ "$1" == "status" ]; then
-	state=$(cat "/sys/class/gpio/gpio$LED_GPIO/value")
-	echo "O estado do LED eh: $state"
-elif [ "$1" == "close" ]; then
-	echo "Desabilitando a GPIO numero $LED_GPIO"
-	echo $LED_GPIO >> "/sys/class/gpio/unexport"
-else
-	echo "Comando nao reconhecido."
-	exit 3                         # erro que indica comando nao reconhecido
+ 
+ 	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO1/direction"
+
+ 	echo "Habilitando a GPIO numero $LED_GPIO2"
+	echo $LED_GPIO2 >> "/sys/class/gpio/export"
+	sleep 1                        # esperar 1 segundo para garantir que a gpio foi exportada
+	
+
+	echo "Acendendo o LED"
+	setLED 1                       # 1 eh recebido como $1 na funcao setLED
+
+  	sleep 2  
+   	echo "Desligando o LED"
+	setLED 0  
+ 
+	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO2/direction"
+  		echo "Desabilitando a GPIO numero $LED_GPIO2"
+    
+	echo $LED_GPIO2 >> "/sys/class/gpio/unexport"
+
+
+	echo "Habilitando a GPIO numero $LED_GPIO3"
+	echo $LED_GPIO3 >> "/sys/class/gpio/export"
+	sleep 1                        # esperar 1 segundo para garantir que a gpio foi exportada
+	
+	echo "Acendendo o LED"
+	setLED 1                       # 1 eh recebido como $1 na funcao setLED
+	sleep 2   
+	echo "Desligando o LED"
+	setLED 0                       # 0 eh recebido como $1 na funcao setLED
+ 
+ 	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO3/direction"
+
+
 fi
