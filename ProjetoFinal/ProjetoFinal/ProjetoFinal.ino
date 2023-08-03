9f20c09232a54e414659bbb658a4ed637af75524
trottlx2 <trottier.louise@aluno.ufabc.edu.br>
2023-08-02 22:40:49 -0300

    projeto

--- ProjetoFinal/ProjetoFinal/ProjetoFinal.ino
+++ ProjetoFinal/ProjetoFinal/ProjetoFinal.ino
@@ -1,13 +1,20 @@
 #include <Thermistor.h>
-const int analogInPin = A0;                  // o potenciômetro esta ligado ao pino A0
+const int analogInPin = A0; // o potenciômetro esta ligado ao pino A0
+
 //Thermistor temp(0);
 float media;
 boolean inicial = true;
 boolean coleta = false;
 int contador = 0;
+int insp_flag = 0;
+int exp_flag = 0;
+int insp_cont = 0;
+int exp_cont = 0;
+
 void setup(){
    // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
    Serial.begin(115200, SERIAL_8N1);
+   pinMode(7, OUTPUT);
 }
 
 void loop(){
@@ -37,19 +44,53 @@ void loop(){
             media = voltagem;
           }
           
-         /* else if (contador = 10){
-            contador = 0;
-            media = voltagem;
-          
-          }*/
           else{
             media = media*0.65 + voltagem*0.35;
-//''            contador = contador + 1;
+     
           }
           int final = (50-((voltagem-media)*1000.0));
+          
           //int final = (((voltagem)*1000.0) - 3000);
-          Serial.println(final);// envia o valor
+          //Serial.println(final);// envia o valor
+          
+          
+       //  int pino_PWM = 23;                         // pwm por software na GPIO23
+       //  int brilho;
+       //  int range = 100;                           // periodo do PWM = 100us*range
+        // wiringPiSetupGpio();                       // usar a numeracao GPIO, nao WPi
+        // pinMode(pino_PWM,OUTPUT);	           // configura GPIO23 como saida
+        // softPwmCreate(pino_PWM, 1, range);         // inicializa PWM por software
+         
+         if (final < 30){
+            insp_flag = 0;
+            exp_flag = 1;
+            exp_cont = 0;
+         }
+         else if( final > 70){
+            insp_flag = 1;
+            exp_flag = 0;
+            insp_cont = 0;
+         }
+         
+         if (insp_flag == 1){
+            insp_cont +=1;
+         }
+         if (exp_flag == 1){
+            exp_cont += 1;
+         }
+         if (exp_cont > 70){
+           digitalWrite(7, HIGH);
+         }
+         //Serial.println("Insp_flag = %d; \tInsp_cont = %d, \tExp_flag = %d, \texp_cont = %d; \tvalor = %d\n",insp_flag, insp_cont, exp_flag, exp_cont, final);
+         Serial.print("Insp_flag:"); Serial.println(insp_flag); 
+         Serial.print("Insp_cont:"); Serial.println(insp_cont);
+         Serial.print("Exp_flag 1:"); Serial.println(exp_flag);
+         Serial.print("exp_cont:"); Serial.println(exp_cont);
+         Serial.print("valor:");Serial.println(final);
+      }
+      
+   delay(100);     
+          
    }
    
-   delay(100);
-}
+   
--- ProjetoFinal/ProjetoFinal_c.c
+++ ProjetoFinal/ProjetoFinal_c.c
@@ -7,7 +7,13 @@
 #include <termios.h>
 #include <string.h>
 #include <math.h>
-#include <>
+//#include <>
+
+int insp_flag = 0;
+int exp_flag = 0;
+int insp_cont = 0;
+int exp_cont = 0;
+
 int main(){
    int file, count;
    if ((file = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY))<0){
@@ -26,43 +32,72 @@ int main(){
    
    while(1){
       if ((count = write(file, "1", 1))<0){
-	 perror("Falha ao escrever na saida.\n");
-	 return -1;
+	      perror("Falha ao escrever na saida.\n");
+	      return -1;
       }   
       usleep(100000);                     // Espera 100ms pela resposta do Arduino
 
       unsigned char receive[6];         // cria um buffer para receber os dados
       if ((count = read(file, (void*)receive, 6))<0){        // recebe os dados
-	 perror("Falha ao ler da entrada\n");
-	 return -1;
+      	 perror("Falha ao ler da entrada\n");
+      	 return -1;
       }
       
       char *subString; // the "result"
       subString = strtok(receive,"\n");
       int resultado = atoi(subString);
-      printf("Valor [%d] \n",resultado);
+      //printf("Valor [%d] \n",resultado);
       if (count==0) printf("Nao houve resposta!\n");
-      else printf("Valor [%d] caracteres: %s\n",resultado,subString);
+      else{
+         //printf("Valor [%d] caracteres: %s\n",resultado,subString);
 
       
-      int pino_PWM = 23;                         // pwm por software na GPIO23
-      int brilho;
-      int range = 100;                           // periodo do PWM = 100us*range
-      wiringPiSetupGpio();                       // usar a numeracao GPIO, nao WPi
-      pinMode(pino_PWM,OUTPUT);	           // configura GPIO23 como saida
-      softPwmCreate(pino_PWM, 1, range);         // inicializa PWM por software
-      
-      if (resultado < 30){
-         printf("Detectado inspiração \n");
-         softPwmWrite (pino_PWM, 255);       
+         int pino_PWM = 23;                         // pwm por software na GPIO23
+         int brilho;
+         int range = 100;                           // periodo do PWM = 100us*range
+         wiringPiSetupGpio();                       // usar a numeracao GPIO, nao WPi
+         pinMode(pino_PWM,OUTPUT);	           // configura GPIO23 como saida
+         softPwmCreate(pino_PWM, 1, range);         // inicializa PWM por software
+         
+         if (resultado < 30){
+            insp_flag = 0;
+            exp_flag = 1;
+            exp_cont = 0;
+         }
+         else if( resultado > 70){
+            insp_flag = 1;
+            exp_flag = 0;
+            insp_cont = 0;
+         }
+         
+         if (insp_flag == 1){
+            insp_cont +=1;
+         }
+         if (exp_flag == 1){
+            exp_cont += 1;
+         }
+         printf("Insp_flag = %d; \tInsp_cont = %d, \tExp_flag = %d, \texp_cont = %d; \tvalor = %d\n",insp_flag, insp_cont, exp_flag, exp_cont, resultado);
       }
-      else{ 
-         printf("brilho [%d] \n",brilho);
-         softPwmWrite (pino_PWM, 0);   
-      }
+         
+         
+         //printf("Detectado expiração \n");
+         //while (1){
+         //   char *subString; // the "result"
+         //   subString = strtok(receive,"\n");
+        //    int resultado = atoi(subString);
+        //    if (resultado < 35 || resultado < 35){
+       //        break;
+         //      }
+        //    }
+       //  softPwmWrite (pino_PWM, 255);       
+      //}
+      //else{ 
+      //   printf("brilho [%d] \n",brilho);
+      //   softPwmWrite (pino_PWM, 0);   
+      // }
 
     //  delay (10);
-      usleep(10000);     
+      usleep(10000); //espera 10ms = 10E-3 s     
    }
    
    close(file);
